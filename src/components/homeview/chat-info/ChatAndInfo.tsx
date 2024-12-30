dc5f56d42291e56a9c6f821334f8954d0ea4d738
Luc Pham <phamtienluc0601@gmail.com>
2024-12-30 18:32:22 +0700

    Update

--- src/components/homeview/chat-info/ChatAndInfo.tsx
+++ src/components/homeview/chat-info/ChatAndInfo.tsx
@@ -3,18 +3,17 @@ import ConversationInfo from "./conversationInfo/ConversationInfo";
 import MainChat from "./mainchat/MainChat";
 
 const ChatAndInfo: React.FC = () => {
-    const [isFullScreen, setFullScreen] = useState(false); // Trạng thái fullScreen
+    const [isFullScreen, setFullScreen] = useState(false);
 
-    // Hàm toggle chuyển đổi giữa 100% và mặc định
     const toggleFullScreen = () => setFullScreen(!isFullScreen);
 
     return (
         <div className="min-h-[96vh] max-h-[96vh] overflow-hidden min-w-[74%] flex flex-row items-center bg-gray-100
             gap-4 pb-0">
-            <div className={`transition-all duration-300 ${isFullScreen ? 'w-full' : 'w-[66%]'}`}>
+            <div className={`transition-all duration-100 ${isFullScreen ? 'w-full' : 'w-[66%]'}`}>
                 <MainChat toggleFullScreen={toggleFullScreen} />
             </div>
-            <div className={`transition-all duration-300 ${isFullScreen ? 'w-0' : 'w-[33%]'}`}>
+            <div className={`transition-all duration-100 ${isFullScreen ? 'w-0' : 'w-[33%]'}`}>
                 <ConversationInfo />
             </div>
         </div>
