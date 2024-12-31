8277aa666036c0fb5e298efce71df3b822cf910a
Luc Pham <phamtienluc0601@gmail.com>
2024-12-31 19:30:39 +0700

    Update profile page

--- src/components/profile/Profile.tsx
+++ src/components/profile/Profile.tsx
@@ -2,10 +2,15 @@ import { BsPencilSquare } from "react-icons/bs";
 import { IoChatbubblesSharp, IoLogoYoutube, IoSettings } from "react-icons/io5";
 import { PiHandWavingFill, PiUploadSimpleFill } from "react-icons/pi";
 import { BiSolidEditAlt } from "react-icons/bi";
-import { FaArrowLeft, FaCameraRetro, FaEdit, FaFacebook, FaInstagramSquare, FaTiktok } from "react-icons/fa";
+import { FaArrowLeft, FaArrowRight, FaCameraRetro, FaEdit, FaFacebook, FaInstagramSquare, FaTiktok } from "react-icons/fa";
 import { useNavigate } from "react-router-dom";
 import { useState } from "react";
 
+interface User {
+    id: number;
+    name: string;
+    avatar: string;
+}
 
 const Profile = () => {
     const [showMenu, setShowMenu] = useState(false);
@@ -16,6 +21,17 @@ const Profile = () => {
     const goBack = () => {
         navigate(-1);
     };
+    const users: User[] = [
+        { id: 1, name: 'Alice', avatar: 'https://images.unsplash.com/photo-1501594907352-04cda38ebc29' },
+        { id: 2, name: 'Bob', avatar: 'https://images.unsplash.com/photo-1531251445707-1f000e1e87d0' },
+        { id: 3, name: 'Charlie', avatar: 'https://images.unsplash.com/photo-1552058544-f2b08422138a' },
+        { id: 4, name: 'David', avatar: 'https://images.unsplash.com/photo-1516534775068-ba3e7458af70' },
+        { id: 5, name: 'Emma', avatar: 'https://images.unsplash.com/photo-1523292562811-8fa7962a78c8' },
+        { id: 6, name: 'Charlie', avatar: 'https://images.unsplash.com/photo-1552058544-f2b08422138a' },
+        { id: 7, name: 'David', avatar: 'https://images.unsplash.com/photo-1516534775068-ba3e7458af70' },
+        { id: 8, name: 'Emma', avatar: 'https://images.unsplash.com/photo-1523292562811-8fa7962a78c8' },
+        { id: 9, name: 'Charlie', avatar: 'https://images.unsplash.com/photo-1552058544-f2b08422138a' },
+    ];
 
     const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
         const file = event.target.files?.[0];
@@ -60,7 +76,7 @@ const Profile = () => {
             <div className="relative flex flex-col w-full min-h-full">
                 <div className="absolute top-4 left-4 z-10">
                     <button className="p-2 rounded-full text-black text-xl bg-gray-200 hover:bg-white"
-                            onClick={goBack}>
+                        onClick={goBack}>
                         <FaArrowLeft />
                     </button>
                 </div>
@@ -85,55 +101,55 @@ const Profile = () => {
                     </div>
 
                     <div className="absolute right-2 bottom-2">
-                    {!showMenu ? (
-                        <button
-                            onClick={() => setShowMenu(true)}
-                            className="items-center text-xl bg-[#e4e6eba0] hover:bg-[#d8dadfcd] p-3 rounded-full"
-                        >
-                            <FaCameraRetro />
-                        </button>
-                    ) : (
-                        <div className="flex flex-col gap-2 text-sm bg-white w-64 shadow-md p-2 rounded-lg">
-                            {/* Tải ảnh lên */}
-                            <div className="w-full p-2 rounded-md hover:bg-gray-200 text-black cursor-pointer">
-                                <label className="flex flex-row gap-2 items-center cursor-pointer">
-                                    <PiUploadSimpleFill />
-                                    <span className="text-black font-semibold">Tải ảnh lên</span>
+                        {!showMenu ? (
+                            <button
+                                onClick={() => setShowMenu(true)}
+                                className="items-center text-xl bg-[#e4e6eba0] hover:bg-[#d8dadfcd] p-3 rounded-full"
+                            >
+                                <FaCameraRetro />
+                            </button>
+                        ) : (
+                            <div className="flex flex-col gap-2 text-sm bg-white w-64 shadow-md p-2 rounded-lg">
+                                {/* Tải ảnh lên */}
+                                <div className="w-full p-2 rounded-md hover:bg-gray-200 text-black cursor-pointer">
+                                    <label className="flex flex-row gap-2 items-center cursor-pointer">
+                                        <PiUploadSimpleFill />
+                                        <span className="text-black font-semibold">Tải ảnh lên</span>
+                                        <input
+                                            type="file"
+                                            accept="image/*"
+                                            className="hidden"
+                                            onChange={handleFileChange}
+                                        />
+                                    </label>
+                                </div>
+                                <div className="w-full p-2 rounded-md hover:bg-gray-200 text-black">
                                     <input
-                                        type="file"
-                                        accept="image/*"
-                                        className="hidden"
-                                        onChange={handleFileChange}
+                                        type="text"
+                                        placeholder="Nhập URL ảnh"
+                                        value={inputUrl}
+                                        onChange={(e) => setInputUrl(e.target.value)}
+                                        className="w-full p-2 border border-gray-300 rounded-md"
                                     />
-                                </label>
+                                </div>
+                                {/* Nhập URL ảnh */}
+                                <div className="flex flex-row gap-2">
+                                    <button
+                                        onClick={handleUpdate}
+                                        className="bg-blue-100 text-blue-500 w-[70%] p-2 rounded-md hover:bg-blue-200"
+                                    >
+                                        Cập nhật
+                                    </button>
+                                    <button
+                                        onClick={() => setShowMenu(false)}
+                                        className="bg-blue-100 text-blue-500 w-[30%] p-2 rounded-md hover:bg-blue-200"
+                                    >
+                                        Huỷ
+                                    </button>
+                                </div>
                             </div>
-                            <div className="w-full p-2 rounded-md hover:bg-gray-200 text-black">
-                                <input
-                                    type="text"
-                                    placeholder="Nhập URL ảnh"
-                                    value={inputUrl}
-                                    onChange={(e) => setInputUrl(e.target.value)}
-                                    className="w-full p-2 border border-gray-300 rounded-md"
-                                />
-                            </div>
-                            {/* Nhập URL ảnh */}
-                            <div className="flex flex-row gap-2">
-                                <button
-                                    onClick={handleUpdate}
-                                    className="bg-blue-100 text-blue-500 w-[70%] p-2 rounded-md hover:bg-blue-200"
-                                >
-                                    Cập nhật
-                                </button>
-                                <button
-                                    onClick={() => setShowMenu(false)}
-                                    className="bg-blue-100 text-blue-500 w-[30%] p-2 rounded-md hover:bg-blue-200"
-                                >
-                                    Huỷ
-                                </button>
-                            </div>
-                        </div>
-                    )}
-                </div>
+                        )}
+                    </div>
                 </div>
 
                 <div>
@@ -149,7 +165,7 @@ const Profile = () => {
                         </div>
 
                         <div className="flex flex-col items-end gap-2 pt-4">
-                            <button className="flex items-center gap-2 py-2 px-4 h-fit w-fit 
+                            {/* <button className="flex items-center gap-2 py-2 px-4 h-fit w-fit 
                                 border-2 border-blue-700 text-blue-700 bg-white 
                                 hover:bg-gradient-to-r from-blue-500 to-purple-400 hover:text-white 
                                 rounded-full shadow-md transition duration-300">
@@ -162,6 +178,20 @@ const Profile = () => {
                                 rounded-full shadow-md transition duration-300">
                                 <PiHandWavingFill />
                                 <p className="font-semibold px-0.5">Gửi kết bạn</p>
+                            </button> */}
+                            <button className="flex items-center gap-2 py-2 px-4 h-fit w-fit 
+                                border-2 border-black text-white bg-black 
+                                hover:bg-gradient-to-r from-white to-gray-200 hover:text-black 
+                                rounded-full shadow-md transition duration-300">
+                                <BiSolidEditAlt />
+                                <p className="font-semibold">Chỉnh sửa</p>
+                            </button>
+                            <button className="flex items-center gap-2 py-2 px-4 h-fit w-fit 
+                                border-2 border-black text-black bg-white 
+                                hover:bg-gradient-to-r from-black to-gray-800 hover:text-white 
+                                rounded-full shadow-md transition duration-300">
+                                <IoSettings />
+                                <p className="font-semibold px-0.5">Cài đặt</p>
                             </button>
                         </div>
                     </div>
@@ -169,21 +199,33 @@ const Profile = () => {
 
                 <div className="flex flex-row justify-between px-8 py-2">
 
-                    <div className="flex flex-row items-start gap-4">
-                        <button className="flex items-center gap-2 py-2 px-4 h-fit w-fit 
-                            border-2 border-black text-white bg-black 
-                            hover:bg-gradient-to-r from-white to-gray-200 hover:text-black 
-                            rounded-full shadow-md transition duration-300">
-                            <BiSolidEditAlt />
-                            <p className="font-semibold">Chỉnh sửa</p>
-                        </button>
-                        <button className="flex items-center gap-2 py-2 px-4 h-fit w-fit 
-                            border-2 border-black text-black bg-white 
-                            hover:bg-gradient-to-r from-black to-gray-800 hover:text-white 
-                            rounded-full shadow-md transition duration-300">
-                            <IoSettings />
-                            <p className="font-semibold px-0.5">Cài đặt</p>
-                        </button>
+                    <div className="flex flex-col items-start justify-between gap-2 max-w-[450px] p-2 rounded-xl bg-blue-50 border-2 border-blue-400">
+                        <p className="font-semibold font-serif">Bạn có tổng cộng 953 người bạn</p>
+                        <div className="flex items-center -space-x-2">
+                            {users.map((user) => (
+                                <div key={user.id} className="relative group">
+                                    <img
+                                        src={user.avatar}
+                                        alt={user.name}
+                                        className="w-10 h-10 rounded-full border-2 border-white cursor-pointer hover:scale-105 transition-transform"
+                                    />
+                                    {/* Tooltip */}
+                                    <div className="absolute -top-8 left-1/2 -translate-x-1/2 bg-gray-800 text-white text-xs px-2 py-1 rounded-md opacity-0 group-hover:opacity-100 transition-opacity">
+                                        {user.name}
+                                    </div>
+                                </div>
+                            ))}
+                        </div>
+                        <div className="flex items-center gap-4">
+                            <p>Lionel Messi, Neymar.JR, Benzema và 950 người khác</p>
+                            <button className="flex gap-2 items-center text-md min-w-fit h-12 border-2 border-blue-700 text-blue-700 bg-white 
+                                hover:bg-gradient-to-r from-blue-500 to-purple-400 hover:text-white 
+                                rounded-full shadow-md transition duration-300 py-2 px-3"
+                            >
+                                <p>Tất cả</p>
+                                <FaArrowRight />
+                            </button>
+                        </div>
                     </div>
 
                     <div className="flex flex-row gap-4 items-center justify-end">
