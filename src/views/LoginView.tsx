a1097b9647f29d428f8134857df763162622f3df
Luc Pham <phamtienluc0601@gmail.com>
2025-01-10 23:37:18 +0700

    Update Login page

--- src/views/LoginView.tsx
+++ src/views/LoginView.tsx
@@ -1,58 +1,68 @@
 import { useState } from 'react';
 
 const LoginView = () => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+	const [email, setEmail] = useState('');
+	const [password, setPassword] = useState('');
 
-  const handleSubmit = (e: any) => {
-    e.preventDefault();
-  };
+	const handleSubmit = (e: any) => {
+		e.preventDefault();
+	};
 
-  return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-100">
-      <div className="bg-white p-8 rounded-lg shadow-lg w-full max-w-md">
-        <h2 className="text-2xl font-bold text-center text-gray-800 mb-6">
-          Login
-        </h2>
-        <form onSubmit={handleSubmit} className="space-y-4">
-          <div>
-            <label className="block text-gray-700">Email</label>
-            <input
-              type="email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              required
-              className="w-full px-4 py-2 mt-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
-            />
-          </div>
-          <div>
-            <label className="block text-gray-700">Password</label>
-            <input
-              type="password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              required
-              className="w-full px-4 py-2 mt-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
-            />
-          </div>
-          <div>
-            <button
-              type="submit"
-              className="w-full bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
-            >
-              Login
-            </button>
-          </div>
-        </form>
-        <p className="mt-4 text-center text-gray-600">
-          Don't have an account?{' '}
-          <a href="#" className="text-blue-500 hover:underline">
-            Sign up
-          </a>
-        </p>
-      </div>
-    </div>
-  );
+	return (
+		<div className="min-h-screen flex gap-8 items-center justify-center bg-white">
+			<div className="bg-[url('/ChatSample.png')] w-[35%] h-[70vh] bg-cover bg-center shadow-xl">
+
+			</div>
+			<div className="p-8 rounded-lg w-full max-w-[544px]">
+				<p className="text-5xl font-bold text-start text-gray-800 mb-16
+					bg-gradient-to-br from-blue-500 to-pink-400 bg-clip-text text-transparent">
+					Nơi tuyệt vời để kết nối năm châu bốn bể
+				</p>
+				<p className="text-md text-start text-gray-800 mb-6">
+					Kết nối với bạn bè và gia đình, xây dựng cộng đồng và đào sâu sở thích của bạn.
+				</p>
+				<form onSubmit={handleSubmit} className="space-y-4">
+					<div>
+						<input
+							type="email"
+							placeholder='Username hoặc email hoặc sđt'
+							value={email}
+							onChange={(e) => setEmail(e.target.value)}
+							required
+							className="w-full px-4 py-2 mt-2 border rounded-xl bg-gray-100 
+								focus:outline-none focus:ring-2 focus:ring-blue-500"
+						/>
+					</div>
+					<div>
+						<input
+							type="password"
+							placeholder='Mật khẩu'
+							value={password}
+							onChange={(e) => setPassword(e.target.value)}
+							required
+							className="w-full px-4 py-2 mt-2 border rounded-xl bg-gray-100 
+								focus:outline-none focus:ring-2 focus:ring-blue-500"
+						/>
+					</div>
+					<div>
+						<button
+							type="submit"
+							className="w-fit bg-blue-500 hover:bg-blue-700 text-white text-lg 
+								font-bold py-2 px-4 rounded-full focus:outline-none focus:ring-2 focus:ring-blue-500"
+						>
+							Login
+						</button>
+					</div>
+				</form>
+				<p className="mt-4 text-center text-gray-600">
+					Chưa có tài khoản?{' '}
+					<a href="#" className="text-blue-500 hover:underline">
+						Đăng ký ngay!
+					</a>
+				</p>
+			</div>
+		</div>
+	);
 };
 
 export default LoginView;
